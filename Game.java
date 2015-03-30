c03efaf05f1a157d00fbc16d95467c0a7d7a3b2c
Kristen Oringer <KrOringer96@gmail.com>
2015-03-30 11:50:11 -0400

    Changed Author

--- Game.java
+++ Game.java
@@ -11,8 +11,8 @@
  *  rooms, creates the parser and starts the game.  It also evaluates and
  *  executes the commands that the parser returns.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2011.08.10
+ * @author  Kristen Oringer
+ * @version 3.30.2015
  */
 
 public class Game 
